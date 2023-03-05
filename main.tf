f8e81e18333f6306c1331bba92678e9b64d0784c
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 08:52:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,16 @@ data "tfe_outputs" "project_list" {
   workspace =  "tfc-project-mgmt"
 }
 
+variable "project_list_var" {
+  type = map
+  default = { data.tfe_outputs.project_list.value }
+}
+
 resource "tfe_workspace" "TFC-Test-Workspace" {
   name         = "TFC-Test-Workspace"
   organization = "TF-Cloud-Demo-OE"
   tag_names    = ["test", "app"]
-  project_id   = lookup(data.tfe_outputs.project_list.values[id], "ProjectX")
+  project_id   = lookup(var.project_list_var[var.id], "ProjectX")
   
 #  data.tfe_outputs.project_list.values(project[ProjectX].id)
 }
