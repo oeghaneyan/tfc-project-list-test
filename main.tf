d08615ba833d8f8d6d75b3b36748b06e6593fe29
omid.eghaneyan <oeghaneyan@gmail.com>
2023-03-05 07:26:07 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+data "tfe_outputs" "project_list" {
+  organization = "TF-Cloud-Demo-OE"
+  workspace =  "tfc-project-mgmt"
+}
+
+resource "null_resource" "project-id" {
+  for_each = toset(data.tfe_outputs.project_list)
+  triggers = {
+    id = each.value.id
+  }
+}
+
+output "project-ids" {
+  value = null_resource.avengers
+}
