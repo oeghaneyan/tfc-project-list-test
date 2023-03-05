0e6e6d06aa961431f4bee01930a8263dcc01cc77
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 08:12:14 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ resource "tfe_workspace" "TFC-Test-Workspace" {
   name         = "TFC-Test-Workspace"
   organization = "TF-Cloud-Demo-OE"
   tag_names    = ["test", "app"]
-  project_id   = "${data.tfe_outputs.project_list.values.project[ProjectX].id}"
+  project_id   = data.tfe_outputs.project_list.values.project[ProjectX].id
 }
