ce5116b2faa5112ca537e733d113c3d7af3dd7c7
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 20:37:17 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,23 +8,6 @@ data "tfe_outputs" "projects" {
   workspace =  "tfc-project-mgmt"
 }
 
-variable "hostname" {
-  description = "Optional- defaults to Terraform Cloud `app.terraform.io`. Normally this is only set for self-managed TFE enviroments."
-  type        = string
-  default = "app.terraform.io"
-}
-
-variable "token" {
-  description = "Token for TFC Organization."
-  type        = string
-  sensitive   = true
-}
-
-variable "org_name" {
-  description = "Name of TFC Organization."
-  type        = string
-}
-
 resource "tfe_workspace" "TFC-Test-Workspace" {
   name         = "TFC-Test-Workspace"
   organization = var.org_name
