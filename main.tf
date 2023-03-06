3fbe30e6991ccfa74458ba4de2874223b77b7086
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 16:39:10 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "tfe_workspace" "TFC-Test-Workspace" {
   organization = "TF-Cloud-Demo-OE"
   tag_names    = ["test", "app"]
 #  project_id   = data.tfe_outputs.projects.values.project_list #["ProjectX"]
-  project_id   = lookup(data.tfe_outputs.projects.values.project_list[value], "ProjectX")
+  project_id   = lookup(data.tfe_outputs.projects.values.project_list[id], "ProjectX")
   
 #  data.tfe_outputs.project_list.values(project[ProjectX].id)
 }
